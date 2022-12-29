0b7fcd7a8a4c4953f169d05c40aa35ba2e8c55d3
nitin shah <ns1215@srmist.edu.in>
2022-12-29 17:42:25 +0530

    O(n)

--- Array/ArrayProblems/02_SubArray.cpp
+++ Array/ArrayProblems/02_SubArray.cpp
@@ -3,5 +3,29 @@ using namespace std;
 
 int main()
 {
+    int n,i;
+    cin>>n;
+    int a[n];
+    int index[n];
+    ;
+    for(i=0;i<n;i++)
+    {
+        cin>>a[i];
+        index[i]=-1;
+    }
+    for(i=0;i<n;i++)
+    {
+        int k=a[i];
+        index[k]=0;
+    }
+    for(i=0;i<n;i++)
+    {
+        if(index[i]==-1)
+        {
+            cout<<i;
+            break;
+        }
+    }
+
     
 }
