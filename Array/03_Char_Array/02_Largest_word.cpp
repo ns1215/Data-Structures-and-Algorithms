4e969916fd7ff2c43a9d6137b2bb56de27dfa336
nitin shah <ns1215@srmist.edu.in>
2023-01-14 10:51:59 +0530

    updated

---
+++ Array/03_Char_Array/02_Largest_word.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;     //size of the string
+    cin.ignore(); 
+    char a[n+1];
+    cin.getline(a,n+1);   // To accept the whole line with spaces
+    cin.ignore();       // To ignore buffer
+    cout<<a<<endl;
+    int i,start=0,count=0,Max_length=0,k=0;
+    for(int i=0;i<=n;i++)
+    {
+        count++;
+        if(a[i] == ' ' || a[i]=='\0')
+        {
+            if(count-1>Max_length)
+            {
+                start=k;
+                Max_length=count-1;
+
+            }
+            k=i+1; 
+            count=0;           
+        }        
+    }
+    for(i=start;i<Max_length+start;i++)
+    cout<<a[i];
+
+    cout<<endl<<"Length="<<Max_length;
+    return 0;
+}
