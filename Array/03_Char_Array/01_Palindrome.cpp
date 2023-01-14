4533b03ee5564fb75da2dc196a5fd19aa36b2261
nitin shah <ns1215@srmist.edu.in>
2023-01-14 09:10:18 +0530

    added

---
+++ Array/03_Char_Array/01_Palindrome.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;     //size of the string
+    char a[n+1];
+    cin>>a;
+    int i=0,check=0;
+    for(int i=0;i<n/2;i++)
+    {
+        if(a[i]=!a[n-1-i])
+        {
+            check=1;
+            cout<<"string is not Palindrome"<<endl;
+            break;
+        }
+    }
+        if(check==0)
+        {
+            cout<<"String is palindrome"<<endl;
+        }
+
+    return 0;
+}
