a7836950008b8c0b6cdd7db91a9417f50ac00ff3
nitin shah <ns1215@srmist.edu.in>
2023-01-14 09:13:45 +0530

    updated

--- Array/03_Char_Array/01_Palindrome.cpp
+++ Array/03_Char_Array/01_Palindrome.cpp
@@ -10,7 +10,7 @@ int main()
     int i=0,check=0;
     for(int i=0;i<n/2;i++)
     {
-        if(a[i]=!a[n-1-i])
+        if(a[i]!=a[n-1-i])
         {
             check=1;
             cout<<"string is not Palindrome"<<endl;
